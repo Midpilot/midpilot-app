1de5c5391d7910156055fd87695303e8900ecb7c
thomassi98 <thomastsi98@gmail.com>
2025-02-05 16:42:13 +0100

    Add dynamic redirect_urls

--- app/api/auth/[kindeAuth]/route.js
+++ app/api/auth/[kindeAuth]/route.js
@@ -1,2 +1,8 @@
 import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
-export const GET = handleAuth();
+
+const redirectConfig = process.env.VERCEL_URL ? {
+  postLoginRedirectURL: `https://${process.env.VERCEL_URL}`,
+  postLogoutRedirectURL: `https://${process.env.VERCEL_URL}`
+} : {};
+
+export const GET = handleAuth(redirectConfig);
