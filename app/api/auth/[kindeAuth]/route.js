72d92adf2868b1bbf9ddfbdc65b1643e026a3e42
thomassi98 <thomastsi98@gmail.com>
2025-02-05 16:51:20 +0100

    Print redirect config

--- app/api/auth/[kindeAuth]/route.js
+++ app/api/auth/[kindeAuth]/route.js
@@ -1,8 +1,10 @@
 import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
 
-const redirectConfig = process.env.VERCEL_URL ? {
+const redirectConfig = process.env.VERCEL_URL && process.env.NODE_ENV === 'development' ? {
   postLoginRedirectURL: `https://${process.env.VERCEL_URL}`,
   postLogoutRedirectURL: `https://${process.env.VERCEL_URL}`
 } : {};
+
+console.log("Redirect config:", redirectConfig);
 
 export const GET = handleAuth(redirectConfig);
