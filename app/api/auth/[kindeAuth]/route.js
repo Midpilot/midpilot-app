2fc6d46ab5258a4f3e17190c7b1a409d6d9abc02
thomassi98 <thomastsi98@gmail.com>
2025-02-05 17:53:24 +0100

    Remove register button

--- app/api/auth/[kindeAuth]/route.js
+++ app/api/auth/[kindeAuth]/route.js
@@ -1,12 +1,3 @@
 import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
 
-const redirectConfig = process.env.VERCEL_URL ? {
-  postLoginRedirectURL: `https://${process.env.VERCEL_URL}`,
-  postLogoutRedirectURL: `https://${process.env.VERCEL_URL}`
-} : {};
-
-console.log("Node env: ", process.env.NODE_ENV);
-console.log("Vercel URL: ", process.env.VERCEL_URL);
-console.log("Redirect config:", redirectConfig);
-
-export const GET = handleAuth(redirectConfig);
+export const GET = handleAuth()
--- app/page.tsx
+++ app/page.tsx
@@ -109,7 +109,7 @@ export default function Home() {
               ) : (
                 <>
                   <LoginLink className="text-sm font-inter text-gray-600 hover:text-gray-900">Sign in</LoginLink>
-                  <RegisterLink className="px-4 py-2 text-sm font-inter text-white bg-black rounded-md hover:bg-gray-800">Sign up</RegisterLink>
+                  {/* <RegisterLink className="px-4 py-2 text-sm font-inter text-white bg-black rounded-md hover:bg-gray-800">Sign up</RegisterLink> */}
                 </>
               )}
             </div>
